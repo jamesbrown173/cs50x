08e76d17afb7c4f84b1019b03f52d47e11e17eed
James Brown <jamesbrown173@gmail.com>
2024-06-12 14:42:43 +0700

    14:42

--- hello.c
+++ hello.c
@@ -2,5 +2,18 @@
 
 int main(void)
 {
-  printf("hello, world! 2 \n");
+
+    int userNumber;
+    char userName[100]; // Declare a character array to store the user name
+    printf("What's your name? \n");
+    fgets(userName, sizeof(userName), stdin); // Use fgets to get input from the user
+    printf("what's your number \n");
+    scanf("%d", &userNumber); // Use fgets to get input number from user
+   
+    if ( userNumber > 10 ) {
+
+    printf("hello, %s\n", userName);
+
+  }
+   
 }
