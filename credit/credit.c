7cb9d4bc1e76bb70d9a521f362c140bbb0150e57
James Brown <jamesbrown173@gmail.com>
2024-06-15 13:30:03 +0700

    ok

--- credit/credit.c
+++ credit/credit.c
@@ -80,7 +80,7 @@ bool check_sum(long num)
 }
 
 
-// Changed repo name to cs50-problems
+// Changed repo name to cs50-problems - LOCAL
   // Calculate the checksum
   
       // Multiply every other digit by 2, 
