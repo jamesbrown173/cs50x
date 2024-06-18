a4522d6e834190381c0d1a401e2f4243d23974a0
James Brown <jamesbrown173@gmail.com>
2024-06-18 15:04:22 +0700

    15:04

---
+++ greet.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+  if (argc == 3)
+  {
+    printf("Hello %s %s! \n", argv[1], argv[2]);
+    return 0;
+  }
+  else
+  {
+    printf("Error: Requires 3 inputs \n");
+    return 1;
+  }
+}
