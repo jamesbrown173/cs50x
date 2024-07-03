2cb5ced5a3da1c5f30a7bf85c4eaab944ba36d0a
James Brown <jamesbrown173@gmail.com>
2024-07-03 14:37:33 +0700

    14:37

---
+++ adresses.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+
+{
+  char *s;
+
+  printf("s: ");
+  scanf("%s", s);
+  printf("s: %s\n", s);
+}
+
+
+
+
+
